89bcd71e4316f94efb44b742c6dcaff34eaa028d
PrabodhPrasad <prabodh.prasad99@gmail.com>
2025-09-10 22:04:22 +0530

    addition of profile to main page

--- static/switch.js
+++ static/switch.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     let currentindex=0;
-    const views=["graph", "map", "currency"];
+    const views=["graph", "map", "currency", "profile"];
     const container=document.getElementById("container");
     window.showview=function(index){
         currentindex=index;
-        container.style.transform=`translateX(-${index * 100}vw)`;
+        container.style.transform=`translateX(-${index*100}vw)`;
     }
     document.getElementById("rightarrow").addEventListener("click", ()=>{
         currentindex=(currentindex+1)%views.length;
